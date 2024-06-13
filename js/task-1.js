b71c9cba3cb8096587cdb9a93f146879d56e690d
mountpeak <126021931+mountpeak@users.noreply.github.com>
2024-06-13 11:03:48 +0300

    Виправив Task1

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,7 @@
 function makeTransaction(quantity, pricePerDroid) {
   const totalPrice = quantity * pricePerDroid;
-  console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
-  return totalPrice;
+  const message = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  return message;
 }
 console.log(makeTransaction(5, 3000));
 console.log(makeTransaction(3, 1000));
